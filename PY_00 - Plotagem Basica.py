3fa35be8b62f043f820c5a67dadfe8544c776457
Gabriel <g.goes01@gmail.com>
2021-05-10 12:42:12 -0300

    Adicionando legenda no gráfico

--- PY_00 - Plotagem Basica.py
+++ PY_00 - Plotagem Basica.py
@@ -4,10 +4,11 @@ x = [0, 1, 2, 3, 4, 5, 6]
 y = [1, 2, 3, 2.5, 4, 5.5, 3]
 
 plt.figure(figsize=(8, 6))
-plt.title("Plot de Desempenho", loc="left", fontsize=20)
+plt.title("Plot de Desempenho", fontsize=20)
 plt.xlabel("Tempo", fontsize="16")
 plt.ylabel("Desempenho", fontsize="16")
 plt.scatter(x, y, linewidths=3, c="gray")
 plt.plot(x, y, linewidth=3, c="gray")
+plt.legend(["Tendência"])
 plt.grid()
 plt.show()
