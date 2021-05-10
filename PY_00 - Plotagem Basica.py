6f00c6955f2532b1e91db3ebb2b6b2d5109cd319
Gabriel <g.goes01@gmail.com>
2021-05-10 12:18:10 -0300

    Limpando o código

--- PY_00 - Plotagem Basica.py
+++ PY_00 - Plotagem Basica.py
@@ -1,22 +1,7 @@
-# # 1 - Primeiro plot
-
 import matplotlib.pyplot as plt
 
 x = [0, 1, 2, 3, 4, 5, 6]
 y = [1, 2, 3, 2.5, 4, 5.5, 3]
-# plt.plot(x, y)
-# plt.show()
-
-# # 2 - Labels
-
-# plt.title("Plot de Desempenho")
-# plt.xlabel("Tempo")
-# plt.ylabel("Desempenho")
-# plt.legend("Desempenho")
-# plt.plot(x, y)
-# plt.show()
-
-# # 3 - Ajustando a estética do plot
 
 plt.figure(figsize=(8, 6))
 plt.title("Plot de Desempenho", loc="left", fontsize=20)
